121485d729b798d80f29372e724648d4bfebf179
Leandro <lesantosads@gmail.com>
2023-11-16 19:05:24 -0300

    New Version

--- WebApplication1/Data/Anotacoes.cs
+++
@@ -1,5 +0,0 @@
-﻿//Entity Framework
-//ORM - Facilita em trabalhar com BD (sql, postgree, oracle). Faz toda estrutura de entidade
-//para dentro do código e depois cria o BD e Tabelas através do codigo, configurados no DBcontext
-//Ex: Usuarios na classe SistemaTarefasDBContext representa uma tabela no BD
-
